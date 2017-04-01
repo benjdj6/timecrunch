3d2e85ed9cdf26df5a13dd7e193705539c3e5ceb
benjdj6 <benjdj6@vt.edu>
2017-03-31 23:04:26 -0400

    Added angularApp.js with factory skeletons

---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,9 @@
+var app = angular.module('timecrunch', ['ui.router']);
+
+app.factory('foods', ['$http', function($http) {
+    //foods methods
+}]);
+
+app.factory('recipes', ['$http', function($http) {
+    //recipes methods
+}]);
