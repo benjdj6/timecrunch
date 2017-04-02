c0024064d606ecf6032c7dde55979677e8f2819c
benjdj6 <benjdj6@vt.edu>
2017-04-02 17:52:05 -0400

    Added foods object in factory and controller for dashboard

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,9 +1,19 @@
 var app = angular.module('timecrunch', ['ui.router']);
 
 app.factory('foods', ['$http', function($http) {
-    //foods methods
+    var o = {
+        foods: []
+    };
 }]);
 
 app.factory('recipes', ['$http', function($http) {
     //recipes methods
+}]);
+
+// Controller for dashboard on home/index
+app.controller('DashCtrl', [
+    '$scope',
+    'foods',
+    function($scope, foods){
+        $scope.foods = foods.foods;
 }]);
