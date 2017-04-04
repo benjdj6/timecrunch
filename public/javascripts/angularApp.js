dd8ac894a60e5791f78b7a3c996d62c794d1104e
benjdj6 <benjdj6@vt.edu>
2017-04-04 18:44:38 -0400

    Created Recipes and NavCtrl controllers and Recipes state

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,17 +1,27 @@
 var app = angular.module('timecrunch', ['ui.router']);
 
 app.config([
-  'stateProvider',
-  'urlRouterProvider',
-  function($stateProvider, $urlRouterProvider) {
-    $stateProvider
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+  $stateProvider
     .state('home', {
       url: '/home',
       templateUrl: '/home.html',
       controller: 'DashCtrl'
+    })
+    .state('food', {
+      url: '/food',
+      templateUrl: '/food.html',
+      controller: 'FoodCtrl'
+    })
+    .state('recipes', {
+      url: '/recipes',
+      templateUrl: '/recipes.html',
+      controller: 'RecipesCtrl'
     });
 
-    $urlRouterProvider.otherwise('home');
+  $urlRouterProvider.otherwise('home');
 }]);
 
 app.factory('foods', ['$http', function($http) {
@@ -21,7 +31,9 @@ app.factory('foods', ['$http', function($http) {
 }]);
 
 app.factory('recipes', ['$http', function($http) {
-  //recipes methods
+  var o = {
+    recipes: []
+  };
 }]);
 
 // Controller for dashboard on home/index
@@ -29,5 +41,40 @@ app.controller('DashCtrl', [
   '$scope',
   'foods',
   function($scope, foods){
+    $scope.foods = foods.foods; 
+}]);
+
+// Controller for dashboard on home/index
+app.controller('FoodCtrl', [
+  '$scope',
+  'foods',
+  function($scope, foods){
     $scope.foods = foods.foods;
+}]);
+
+// Controller for dashboard on home/index
+app.controller('RecipesCtrl', [
+  '$scope',
+  'recipes',
+  function($scope, recipes){
+    $scope.recipes = recipes.recipes;
+}]);
+
+// Controller for dashboard on home/index
+app.controller('NavCtrl', [
+  '$scope',
+  '$state',
+  function($scope, $state){
+    
+    $scope.home = function() {
+      $state.go('home');
+    };
+
+    $scope.food = function() {
+      $state.go('food');
+    };
+
+    $scope.recipes = function() {
+      $state.go('recipes');
+    };
 }]);
