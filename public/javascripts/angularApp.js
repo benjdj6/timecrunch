3d75a3b6c49aa831b5bac796f534571f19d03d75
benjdj6 <benjdj6@vt.edu>
2017-04-03 23:41:17 -0400

    Added initial "home" state

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,19 +1,33 @@
 var app = angular.module('timecrunch', ['ui.router']);
 
+app.config([
+  'stateProvider',
+  'urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'DashCtrl'
+    });
+
+    $urlRouterProvider.otherwise('home');
+}]);
+
 app.factory('foods', ['$http', function($http) {
-    var o = {
-        foods: []
-    };
+  var o = {
+    foods: []
+  };
 }]);
 
 app.factory('recipes', ['$http', function($http) {
-    //recipes methods
+  //recipes methods
 }]);
 
 // Controller for dashboard on home/index
 app.controller('DashCtrl', [
-    '$scope',
-    'foods',
-    function($scope, foods){
-        $scope.foods = foods.foods;
+  '$scope',
+  'foods',
+  function($scope, foods){
+    $scope.foods = foods.foods;
 }]);
