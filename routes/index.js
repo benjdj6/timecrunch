4c3cceaebb2f49e8c180559f13707f638e6e282a
benjdj6 <benjdj6@vt.edu>
2017-04-05 19:52:45 -0400

    Removed GET food and GET recipes routes

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// GET home page.
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Time Crunch' });
-});
-
-/* GET food list page. */
-router.get('/food', function(req, res, next) {
-  res.render('food', { title: 'Time Crunch - Food' });
-});
-
-/* GET food list page. */
-router.get('/recipes', function(req, res, next) {
-  res.render('recipes', { title: 'Time Crunch - Recipes' });
 });
 
 module.exports = router;
