e0d738e9492b4453528276565a23cfebdd608c0e
benjdj6 <benjdj6@vt.edu>
2017-04-05 19:57:09 -0400

    Added POST food route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+var Food = mongoose.model('Food');
+var Recipe = mongoose.model('Recipe');
+
 // GET home page.
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Time Crunch' });
 });
+
+// POST food
+router.post('/food', function(req, res, next) {
+    var food = new Food(req.body);
+
+    food.save(function(err, food){
+        if(err) {
+            return next(err);
+        }
+
+        res.json(food);
+    });
+})
 
 module.exports = router;
