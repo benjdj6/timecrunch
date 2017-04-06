1511691e4b9afc2e81180af521bc0dc726aa554d
benjdj6 <benjdj6@vt.edu>
2017-04-06 18:47:14 -0400

    Added get all food route

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Time Crunch' });
 });
 
+// GET all food
+router.get('/food', function(req, res, next) {
+    Food.find(function(err, food){
+        if(err){
+            return next(err);
+        }
+        res.json(food);
+    });
+})
+
 // POST food
 router.post('/food', function(req, res, next) {
     var food = new Food(req.body);
