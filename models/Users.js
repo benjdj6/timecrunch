42911038eafd5dbbfac5a4f7ec36519af024192b
benjdj6 <benjdj6@vt.edu>
2017-04-17 22:25:53 -0400

    Added setPassword function to Users model

--- models/Users.js
+++ models/Users.js
@@ -6,4 +6,10 @@ var UserSchema = new mongoose.Schema({
     salt: String
 });
 
+UserSchema.methods.setPassword = function(password) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
 mongoose.model('User', UserSchema);
