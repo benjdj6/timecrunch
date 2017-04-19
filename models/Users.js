22006c3b4d1c7af43ff15931087212618109b633
benjdj6 <benjdj6@vt.edu>
2017-04-18 21:51:20 -0400

    Added password validation function to user

--- models/Users.js
+++ models/Users.js
@@ -13,4 +13,10 @@ UserSchema.methods.setPassword = function(password) {
     this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
 };
 
+UserSchema.methods.validPassword = function(password) {
+    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+
+    return this.hash === hash;
+};
+
 mongoose.model('User', UserSchema);
