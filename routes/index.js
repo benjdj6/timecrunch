e14e6fb022fdf6c85224960e646f6727aa20768f
benjdj6 <benjdj6@vt.edu>
2017-05-05 20:40:43 -0400

    Added user registration route to index.js

--- routes/index.js
+++ routes/index.js
@@ -109,4 +109,25 @@ router.delete('/recipes/:recipe', function(req, res, next) {
     });
 });
 
+// POST new user
+router.post('/register', function(req, res, next) {
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).json({ message: 'Please fill out all fields' });
+    }
+
+    var user = new User();
+
+    user.username = req.body.username;
+
+    user.setPassword(req.body.password);
+
+    user.save(function(err) {
+        if (err) {
+            return next(err);
+        }
+        
+        return res.json({ token: user.generateJWT() });
+    });
+});
+
 module.exports = router;
