06b0955a57778dfa52c6766710d6ff49a03b11c6
benjdj6 <benjdj6@vt.edu>
2017-05-08 19:29:23 -0400

    Created login state

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -45,6 +45,16 @@ function($stateProvider, $urlRouterProvider) {
         }]
       }
     })
+    .state('login', {
+      url: '/login',
+      templateUrl: '/login.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'auth', function($state, auth) {
+        if(auth.isLoggedIn()) {
+          $state.go('home');
+        }
+      }]
+    })
     .state('register', {
       url: '/register',
       templateUrl: '/register.html',
