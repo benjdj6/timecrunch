ea51cbac2441773beebd357e8ad9130316bfbad2
benjdj6 <benjdj6@vt.edu>
2017-05-09 21:28:55 -0400

    Added jwt to index.js

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,9 @@ var User = mongoose.model('User');
 
 var passport = require('passport');
 
+var jwt = require('express-jwt');
+var auth = jwt({ secret: process.env.SECRET, userProperty: 'payload' });
+
 // GET home page.
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'time crunch' });
