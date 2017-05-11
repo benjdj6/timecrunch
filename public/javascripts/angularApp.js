eeb3ab03deb21e0458cee06b1ea6cb40e8c8557d
benjdj6 <benjdj6@vt.edu>
2017-05-10 21:02:45 -0400

    Added currentUser to NavCtrl

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -355,5 +355,6 @@ app.controller('NavCtrl', [
   function($scope, auth){
     
     $scope.isLoggedIn = auth.isLoggedIn;
+    $scope.currentUser = auth.currentUser;
     $scope.logOut = auth.logOut;
 }]);
