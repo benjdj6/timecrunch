be81ae4139976476750a77e782ccacd5759b5808
benjdj6 <benjdj6@vt.edu>
2017-06-01 21:27:42 -0400

    Ingredients now link to recipe

--- models/Ingredients.js
+++ models/Ingredients.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var IngredientSchema = new mongoose.Schema({
     name: String,
     amount: {type: Number, default: 1},
-    units: String
+    units: String,
+    recipe: {type: mongoose.Schema.Types.ObjectId, ref: 'Recipe'}
 });
 
 mongoose.model('Ingredient', IngredientSchema);
