2c61c73e8a959721789b493c7e2a761509e3bdec
benjdj6 <benjdj6@vt.edu>
2017-07-17 21:16:48 -0400

    Moved ingredient factory to separate file

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -274,26 +274,6 @@ app.factory('recipes', ['$http', 'auth', function($http, auth) {
   return o;
 }]);
 
-// Factory for ingredients handling related functions
-app.factory('ingredients', ['$http', function($http){
-  let o = {};
-  
-  // Create a new ingredient
-  o.create = function(recipeID, ingredient) {
-    return $http.post('/recipes/' + recipeID + '/ingredients',
-      ingredient).then(function(data) {
-        return data.data;
-    });
-  };
-
-  // Delete an ingredient
-  o.delete = function(recipeID, ingredient) {
-    return $http.delete('/recipes/' + recipeID + 'ingredients/' + ingredient._id);
-  };
-
-  return o;
-}]);
-
 // Controller for dashboard on home/index
 app.controller('DashCtrl', [
   '$scope',
