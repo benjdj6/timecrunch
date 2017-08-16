fc8e28dd1a6944ee6bb52100086e1a5cc20a273b
benjdj6 <benjdj6@vt.edu>
2017-08-15 21:18:33 -0700

    Added universal unit value to IngredientSchema

--- models/Ingredients.js
+++ models/Ingredients.js
@@ -4,6 +4,7 @@ var IngredientSchema = new mongoose.Schema({
   name: String,
   amount: {type: Number, default: 1},
   units: String,
+  universal_unit: {type: Number, default: 1},
   recipe: {type: mongoose.Schema.Types.ObjectId, ref: 'Recipe'}
 });
 
